f67d3d038c084e5ea61635cb18eb871814edb3be
TetianaBu <tetianataker@gmail.com>
2020-08-09 12:40:47 +0200

    Add static

--- explainer_project/convention/views.py
+++ explainer_project/convention/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
+class IndexView(CreateView):
+    title = 'Test'
+    template_name = 'index.html'
--- explainer_project/explainer_project/settings.py
+++ explainer_project/explainer_project/settings.py
@@ -31,15 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.CoursesConfig',
+    'convention.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
-    'convention',
-    
 ]
 
 MIDDLEWARE = [
